f9ccbf01354d5c55e94df0d2cc0910bb8c83bd23
zzheng92 <zzheng92@ad.engr.wisc.edu>
2023-11-24 15:51:21 -0800

    debug package requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    required_packages = f.read().splitlines()
+
 setup(
     name='spotiphy',
     version='0.1.0',
@@ -11,16 +14,5 @@ setup(
     description='An integrated pipeline designed to deconvolute and decompose spatial transcriptomics data, '
                 'and produce pseudo single-cell resolution images.',
     python_requires=">=3.8.5",
-    install_requires=[
-        "pip",
-        "torch",
-        "pandas",
-        "numpy",
-        "scipy",
-        "matplotlib",
-        "seaborn",
-        "scanpy",
-        "scikit-learn",
-        "tqdm",
-    ]
+    install_requires=required_packages
 )
