2fa081e8f071ec65039a65c234f75f722b36bdc8
zzheng92 <zzheng92@ad.engr.wisc.edu>
2023-11-24 19:15:26 -0800

    regular update

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 from setuptools import setup
 
-with open('requirements.txt') as f:
-    required_packages = f.read().splitlines()
+required_packages = [
+    "squidpy==1.2.3",
+    "scanpy==1.9.3",
+    "scikit-learn==1.2.2",
+    "scipy==1.9.1",
+    "pandas==2.0.0",
+    "numpy==1.22.4",
+    "matplotlib",
+    "seaborn",
+    "tqdm",
+    "charset-normalizer==3.1.0",
+    "tensorflow==2.12.0",
+    "stardist==0.8.3",
+    "torch",
+    "pyro-ppl==1.8.4",
+    "opencv-python>=4.7",
+    "jupyter"
+]
 
 setup(
     name='spotiphy',
@@ -13,6 +29,6 @@ setup(
     author_email='zzheng92@wisc.edu',
     description='An integrated pipeline designed to deconvolute and decompose spatial transcriptomics data, '
                 'and produce pseudo single-cell resolution images.',
-    python_requires=">=3.8.5",
+    python_requires=">=3.9",
     install_requires=required_packages
 )
